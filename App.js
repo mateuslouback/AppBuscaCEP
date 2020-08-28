afbdac799e056a2db629a3f67daf27ba204c6502
Mateus Louback <mateuslouback@MacBook-de-Mateus.local>
2020-08-28 15:02:41 -0300

    Add Map e Input

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from "react";
+import MapView, { Marker } from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={{
+            latitude: -21.7808787,
+            longitude: -43.3589147,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <SafeAreaView style={styles.safeArea}>
+            <TextInput
+              style={styles.input}
+              placeholder="Digite o CEP aqui"
+              keyboardAppearance="dark"
+              keyboardType="number-pad"
+              returnKeyType={"done"}
+            />
+          </SafeAreaView>
+          <Marker
+            coordinate={{ latitude: -21.7808787, longitude: -43.3589147 }}
+          />
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  input: {
+    width: "auto",
+    height: 55,
+    backgroundColor: "#FFF",
+    borderColor: "#EEE",
+    borderWidth: 4,
+    marginTop: 10,
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    fontSize: 14,
+    marginHorizontal: 25,
   },
 });
